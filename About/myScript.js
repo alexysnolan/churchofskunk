db70678cdac5474f34228877ad06e8d6c118b5d4
alexysnolan <alexysknolan@gmail.com>
2026-03-07 21:30:58 -0500

    faq

--- About/myScript.js
+++ About/myScript.js
@@ -6,3 +6,25 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+
+
+
+
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
