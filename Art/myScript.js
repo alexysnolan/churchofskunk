5f026fb57b64ceeec9c71c35f42a3172957ff54f
alexysnolan <alexysknolan@gmail.com>
2026-03-19 17:24:43 -0400

    misc

--- Art/myScript.js
+++ Art/myScript.js
@@ -6,3 +6,32 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+
+async function loadQuotesFromFile(bushisms.txt) {
+    const response = await fetch(bushisms.txt);
+    const text = await response.text();
+    return text 
+      .split('\n')
+      .map(line => line.trim())
+      .filter(line => line !=='');
+}
+
+async function showRandomQuote() {
+  const quotes = await
+loadQuotesFromFile('bushisms.txt');
+  const randomQuote =
+quotes[Math.floor(Math.random() *
+quotes.length)];
+  document.getElementsById('quoteBox').textContent = randomQuote;
+}
+
+
+
+
+
+function randomlist(list) {
+  var x = Math.floor(Math.random() * list.length);
+  return list[x];
+}
